9e3cd2e148de942fa3cca2904a716d0c758a7b63
Gabriel Silveira <ramos97033@outlook.com>
2025-09-21 18:21:43 -0300

    feat: making projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,9 +1,47 @@
+import { ArrowLeft } from "lucide-react"
+import { Table,TableHeader, TableRow, TableHead, TableBody, TableCell } from "../components/ui/table"
+import Link from "next/link"
 const ProjectsPage = () => {
     return(
+        <>
+        <Link href="/">
+        <section className="text-green flex gap-1 items-center p-5 cursor-pointer">
+            <ArrowLeft className="hover:-translate-x-1 transition-all" />
+            <p className="text-sm hover:underline underline-offset-4 transition-all duration-200">Voltar</p>            
+        </section>
+        </Link>
         <main className="flex flex-col justify-center items-center text-green py-10">
             <h1 className="text-3xl" >Projetos</h1>
-            <p>Projetos</p>
+            <section className="w-full px-32">
+            <Table className="">
+                <TableHeader >
+                    <TableRow className="border-green">
+                        <TableHead>Projeto</TableHead>
+                        <TableHead>Link</TableHead>
+                        <TableHead>Tecnologia</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody className="rounded" >
+                    <TableRow className="border-green cursor-pointer hover:bg-gray-800">
+                        <TableCell>Need</TableCell>
+                        <TableCell><Link target="_blank" className="hover:underline underline-offset-4" href="https://need-web-gerencial.vercel.app/">https://need-web-gerencial.vercel.app/</Link></TableCell>
+                        <TableCell>tetst</TableCell>
+                    </TableRow>
+                    <TableRow className="border-green cursor-pointer hover:bg-gray-800">
+                        <TableCell>Soc</TableCell>
+                        <TableCell>Link</TableCell>
+                        <TableCell>tetst</TableCell>
+                    </TableRow>
+                    <TableRow className="border-green cursor-pointer hover:bg-gray-800 ">
+                        <TableCell>Soc</TableCell>
+                        <TableCell>texte</TableCell>
+                        <TableCell>tetst</TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+            </section>
         </main>
+        </>
     )
 }
 
